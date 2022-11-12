fef275b406bb0d819cc6813a0034da23b8d1dcc7
Claudio Campos <callcocam@gmail.com>
2022-11-11 23:06:09 -0300

    automatico

--- src/Models/Concerns/HasCover.php
+++ src/Models/Concerns/HasCover.php
@@ -4,7 +4,7 @@
 * User: callcocam@gmail.com, contato@sigasmart.com.br
 * https://www.sigasmart.com.br
 */
-namespace Tall\Orm\Models;
+namespace Tall\Orm\Models\Concerns;
 
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
